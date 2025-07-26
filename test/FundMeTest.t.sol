4a7c1f6fa1f1d93e498ba68ade0cf67472c41380
John-Mukhwana <johnbradill67@gmail.com>
2025-07-26 17:19:17 +0300

    Add initial FundMeTest contract with setup and demo test functions

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,23 @@
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.30;
+
+import "forge-std/Test.sol";
+
+contract FundMeTest is Test {
+    
+    uint256 favNumber=0;
+    bool greatCourse=false;
+    // This function is called before each test
+     function setUp() external {
+            // Initialize variables or set up the environment for tests
+            favNumber = 42;
+            greatCourse = true;
+     }
+
+     function testDemo() public{
+          assertEq(favNumber,45);
+          assertEq(greatCourse,true);
+     }
+}
