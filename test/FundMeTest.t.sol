6f8d344fff376a9a9b6ab5e82a249f69613c7d2e
John-Mukhwana <johnbradill67@gmail.com>
2025-07-26 17:20:44 +0300

    Refactor FundMeTest contract for code style consistency and correct test assertions

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -1,4 +1,3 @@
-
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.30;
@@ -6,18 +5,18 @@ pragma solidity ^0.8.30;
 import "forge-std/Test.sol";
 
 contract FundMeTest is Test {
-    
-    uint256 favNumber=0;
-    bool greatCourse=false;
-    // This function is called before each test
-     function setUp() external {
-            // Initialize variables or set up the environment for tests
-            favNumber = 42;
-            greatCourse = true;
-     }
+    uint256 favNumber = 0;
+    bool greatCourse = false;
 
-     function testDemo() public{
-          assertEq(favNumber,45);
-          assertEq(greatCourse,true);
-     }
+    // This function is called before each test
+    function setUp() external {
+        // Initialize variables or set up the environment for tests
+        favNumber = 42;
+        greatCourse = true;
+    }
+
+    function testDemo() public {
+        assertEq(favNumber, 42);
+        assertTrue(greatCourse);
+    }
 }
