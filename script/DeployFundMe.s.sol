d8c815c757b94f58ff2364dc1a8ec68fbcb916a3
John-Mukhwana <johnbradill67@gmail.com>
2025-07-26 18:11:49 +0300

    Refactor DeployFundMe contract to implement Script interface for deployment

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -5,10 +5,10 @@ pragma solidity ^0.8.30;
 import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundeMe.sol";
 
-contract DeployFundMe {
-    FundMe public fundMe;
-
-    constructor() {
-        fundMe = new FundMe(0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38);
+contract DeployFundMe is Script {
+    function run() external{
+        vm.startBroadcast();
+        new FundMe(0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38);
+        vm.stopBroadcast();
     }
 }
