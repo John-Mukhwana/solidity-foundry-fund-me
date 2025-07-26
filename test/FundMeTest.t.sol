cccf32ccff3d4d647b4d231841d5f810283b0688
John-Mukhwana <johnbradill67@gmail.com>
2025-07-26 17:28:30 +0300

    Enhance FundMeTest contract by improving import statement and adding console logs for better test output visibility

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -2,21 +2,23 @@
 
 pragma solidity ^0.8.30;
 
-import "forge-std/Test.sol";
+
+import {Test,console} from "forge-std/Test.sol";
 
 contract FundMeTest is Test {
     uint256 favNumber = 0;
     bool greatCourse = false;
+   
 
     // This function is called before each test
     function setUp() external {
         // Initialize variables or set up the environment for tests
         favNumber = 42;
         greatCourse = true;
+        console.log("This will get printed first!");
     }
 
-    function testDemo() public {
-        assertEq(favNumber, 42);
-        assertTrue(greatCourse);
+       function testMinimumDollarIsFive() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
     }
 }
